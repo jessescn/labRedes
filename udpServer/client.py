4d7310f3a644423720463b96b57b03d87a09529e
Jessé Souza <jesse.neto@ccc.ufcg.edu.br>
2019-10-29 23:17:53 -0300

    change udp client size, now client is a class

--- udpServer/client.py
+++ udpServer/client.py
@@ -3,53 +3,61 @@ import time
 from threading import Thread
 from queue import Queue
 
-def recv_ack(socket, sentence):
-    print('waiting ack from server')
-    message = socket.recvfrom(1024)
-    sentence.put(message)
 
-def handle_connection(client_socket, dest, sentence, sucess_operation):
-    confirmations = Queue()
+class ThreadClient(object):
 
-    while True:
+    def __init__(self, server_ip, server_port):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.dest = ( server_ip, server_port)
 
-        client_socket.sendto(sentence.encode('utf-8'), dest)
+    def execute(self):
+        
+        user_operation = input('Write some operation: ')
 
-        action_thread = Thread(target=recv_ack, args=(client_socket, confirmations))
-        action_thread.start()
-        action_thread.join(timeout=0.1)
+        self.user_operation = user_operation
 
-        if confirmations.qsize() > 0 and confirmations.get()[0].decode('utf-8') == 'ACK':
-            print('received ack from server!')
-            break
-    
-    response = client_socket.recvfrom(1024)
+        while True:
+            responses = Queue()
 
-    print("Resut:", response[0].decode('utf-8'))
+            connection_thread = Thread(target=self.handle_connection, args=(responses, ))
+            connection_thread.start()
+            connection_thread.join(timeout=2)
 
-    sucess_operation.put(response)
+            if responses.qsize() > 0:
+                break
 
-def run_client(server_ip, server_port):
 
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    dest = ( server_ip, server_port)
-    
-    sentence = input('Write some operation: ')
+    def handle_connection(self, sucess_operation):
 
-    while True:
+        confirmations = Queue()
 
-        responses = Queue()
+        while True:
+            print('sending request to server!')
+            self.socket.sendto(self.user_operation.encode('utf-8'), self.dest)
 
-        connection_thread = Thread(target=handle_connection, args=(client_socket, dest, sentence, responses))
-        connection_thread.start()
-        connection_thread.join(timeout=2)
+            action_thread = Thread(target=self.recv_ack, args=(confirmations,))
+            action_thread.start()
+            action_thread.join(timeout=0.1)
 
-        if responses.qsize() > 0:
-            break
+            if confirmations.qsize() > 0 and confirmations.get()[0].decode('utf-8') == 'ACK':
+                print('received ack from server!')
+                break
+        
+        response = self.socket.recvfrom(1024)
+
+        if response[0].decode('utf-8') != 'ACK':
+            print('Received response from server!')
+            print("\nResult: {}\n".format(response[0].decode('utf-8')))
+            sucess_operation.put(response)        
+
+    def recv_ack(self, sentence):
+        print('waiting ack from server')
+        message = self.socket.recvfrom(1024)
+        sentence.put(message)
 
 if __name__ == '__main__':
 
     SERVER_IP = "localhost"
     SERVER_PORT = 5000
 
-    run_client(SERVER_IP, SERVER_PORT)
+    ThreadClient(SERVER_IP, SERVER_PORT).execute()
